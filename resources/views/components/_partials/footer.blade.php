c7a04eca5f3f586c9d7bfb96ad744409952bbb59
Arjunstein <arjun.gnw09@gmail.com>
2025-07-19 21:40:05 +0700

    💄 slicing layout

---
+++ resources/views/components/_partials/footer.blade.php
@@ -0,0 +1,6 @@
+{{-- Footer  --}}
+{{-- <p class="my-10 text-sm text-center text-gray-500">
+    &copy; {{ date('Y') }} <a href="#" class="hover:underline"
+        target="_blank">{{ config('app.name') }}</a>. All rights reserved.
+</p> --}}
+{{-- Footer end --}}
